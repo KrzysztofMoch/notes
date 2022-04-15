bf18387dbffed06c90ccd559bb957513353c9c9e
MochKrzysztof <krzysmoch.programs@gmail.com>
2022-04-15 12:33:50 +0200

    add color files

---
+++ src/common/colors.ts
@@ -0,0 +1,6 @@
+const APP_COLORS = {
+  white: '#fff',
+  black: '#16161e',
+};
+
+export default APP_COLORS;
---
+++ src/common/themes.ts
@@ -0,0 +1,9 @@
+const APP_THEMES = {
+    BLUE: {
+        primary: '#9ebedb',
+        secondary: '#255585',
+        fontColor: '#fff',
+    }
+}
+
+export default APP_THEMES;
