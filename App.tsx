4d77844691ce14f4d2960589038f1821ac26997c
MochKrzysztof <krzysmoch.programs@gmail.com>
2022-04-27 21:32:18 +0200

    add SaveAreaView

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -8,7 +9,7 @@ import MainNavigation from './src/navigation/MainNavigation';
 import { store, persistor } from './src/redux/store';
 
 const App = () => (
-  <View style={StyleSheet.absoluteFill}>
+  <SafeAreaView style={StyleSheet.absoluteFill}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <GestureHandlerRootView style={StyleSheet.absoluteFill}>
@@ -16,7 +17,7 @@ const App = () => (
         </GestureHandlerRootView>
       </PersistGate>
     </Provider>
-  </View>
+  </SafeAreaView>
 );
 
 export default App;
